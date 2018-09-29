29f036ea453687543c77e7e9a48eaeb12e31192a
Jonathan Koss <jonathan_koss@yahoo.com>
2018-09-29 21:16:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,4 +3,12 @@ function happyHolidays() {
   return "Happy holidays!";
 }
 
-/*  */
+function happyHolidaysTo(name) {
+  return `Happy holidays, ${name}!`
+}
+
+/*  describe('happyHolidaysTo(name)', () => {
+    it('returns "Happy holidays, ${name}!"', () => {
+      expect(happyHolidaysTo('you')).toEqual('Happy holidays, you!')
+    })
+  }) */
