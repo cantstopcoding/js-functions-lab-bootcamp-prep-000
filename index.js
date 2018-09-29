e57f826fad071a19d7d886fb327cf261d3d3b9b4
Jonathan Koss <jonathan_koss@yahoo.com>
2018-09-29 21:32:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,4 +11,8 @@ function happyHolidayTo(holiday, name) {
   return `Happy ${holiday}, ${name}!`;
 }
 
+function holidayCountdown(days, holiday) {
+  return `It's ${days} days until ${holiday}`;
+}
+
 /*   */
